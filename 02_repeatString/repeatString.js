5368fe39bc97b3746730211f255c9eef8787252d
Fossu <a.brejzek@gmail.com>
2022-05-21 15:22:11 +0200

    Solution to ex.2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (string, n = 1) {
+  if (n < 0) {
+    return 'ERROR'
+  }
+  let longString = "";
+  for (let i = 1; i <= n; i++) {
+    longString = longString.concat(string);
+  }
+  return longString;
 };
 
 // Do not edit below this line
