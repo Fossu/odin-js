7ae931f882cf009903957e78d4f9a0065c7a4d0a
Fossu <a.brejzek@gmail.com>
2022-05-21 20:32:25 +0200

    Solution to ex.9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  string = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "").replace(/\s/g, "");
+  string = string.toLowerCase();
+  string = string.split("");
+  if (string.toString() == string.reverse().toString()) return true;
+  return false;
 };
 
 // Do not edit below this line
