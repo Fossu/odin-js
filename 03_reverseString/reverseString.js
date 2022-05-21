49f44e13e023a7d344967515bee889c18aef3cd5
Fossu <a.brejzek@gmail.com>
2022-05-21 15:48:45 +0200

    Solution to ex.3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  const elementArray = string.split("");
+  const reverseArray = elementArray.reverse();
+  const reverseString = reverseArray.join("");
+  return reverseString;
 };
 
+console.log(reverseString('Adam is cool.'));
 // Do not edit below this line
 module.exports = reverseString;
