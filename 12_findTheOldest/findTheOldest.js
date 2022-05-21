1997df1dfa9abbbccdaa91c2ea776053a571b67f
Fossu <a.brejzek@gmail.com>
2022-05-21 21:11:18 +0200

    Solution to ex.12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (array) {
+  let ages = [];
+  const currentYear = new Date().getFullYear();
+  for (let i = 0; i < array.length; i++) {
+    if (array[i]['yearOfDeath'] == undefined) {
+      ages.push(currentYear - array[i]['yearOfBirth']);
+    } else {
+      ages.push(array[i]['yearOfDeath'] - array[i]['yearOfBirth']);
+    }
+  }
+  return array[ages.indexOf(Math.max(...ages))];
 };
 
 // Do not edit below this line
