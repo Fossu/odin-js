9d8f968929ba9f9659059534898dc70a26033105
Fossu <a.brejzek@gmail.com>
2022-05-21 16:10:19 +0200

    Solution to ex.4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...toRemove) {
+  for (const element of toRemove) {
+    // console.log(array.indexOf(element));
+    if (array.indexOf(element) >= 0) {
+      // Starting at element index remove 1 array element
+      array.splice(array.indexOf(element), 1);
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
