30d8708bca33946fb9a01a3b2aae03c40a55dd1e
Fossu <a.brejzek@gmail.com>
2022-05-21 20:41:14 +0200

    Solution to ex.10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+  n = Number(n);
+  if (n <= 0) {
+    return 'OOPS';
+  }
+  let fibonacci1 = 1;
+  let fibonacci2 = 1;
+  let placeholder;
+  for (let i = 2; i < n; i++) {
+    placeholder = fibonacci2;
+    fibonacci2 = fibonacci1 + fibonacci2;
+    fibonacci1 = placeholder;
+  }
+  return fibonacci2;
 };
 
 // Do not edit below this line
