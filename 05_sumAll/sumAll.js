c6912463ca8a1a66817e2abab3fe0c949860a72a
Fossu <a.brejzek@gmail.com>
2022-05-21 16:20:06 +0200

    Solution to ex.5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function (x, y) {
+  let sum = 0;
+  const upper = Math.max(x, y);
+  const lower = Math.min(x, y);
+  if (lower < 0 || typeof (x) != 'number' || typeof (y) != 'number') {
+    return 'ERROR';
+  }
+  for (let i = 0; i <= (upper - lower); i++) {
+    sum = sum + lower + i;
+  }
+  return sum;
 };
 
+console.log(typeof (3));
 // Do not edit below this line
 module.exports = sumAll;
